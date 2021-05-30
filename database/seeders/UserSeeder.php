ce7aec60960856dcda5550a4d9d144ac14a1cc54
HtetAungShane1998 <htetaungshane2k202@gmail.com>
2021-05-30 20:25:15 +0630

    first commit

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table('articles')->insert([
+            'title' => Str::random(10),
+            'body' => Str::random(100),
+            'category_id' => Str::random(10),
+        ]);
+    }
+}
